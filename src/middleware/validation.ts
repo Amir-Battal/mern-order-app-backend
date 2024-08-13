ac454cc42be68eb1d7a9dd33d8bf5342fad484a1
Amir-Battal <ameerbatttal@hotmail.com>
2024-08-13 20:50:19 +0300

    Cuisines

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -28,5 +28,4 @@ export const validateMyRestaurantRequest =[
     body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
     body("menuItems.*.price").isFloat({ min: 0 }).withMessage("Menu item price is required and must be a positive number"),
     handleValidationErrors,
-
 ];
